4110b73d3ce9f2ff0d7090799c8bebd371a79b46
Arshia <arshiahsn@gmail.com>
2021-03-29 12:47:39 -0600

    socket added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 const PORT = 8080;
 
 http.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
+});
+
+io.on('connection', (socket) => { /* socket object may be used to send specific messages to the new connected client */
+
+    console.log('new client connected');
 });

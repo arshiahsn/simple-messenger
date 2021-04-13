57af88d5d453f8c338d85eba7e22637fb4451515
Arshia <arshiahsn@gmail.com>
2021-04-13 11:27:47 -0600

    added .env for mongoURI

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const keys = require("./keys");
 
-
+const db = require('db')
+db.connect({
+  mongoURI: process.env.DB_URI
+})
 // Replace this with your MONGOURI.
-const MONGOURI = keys.mongoURI;
+const MONGOURI = mongoURI;
 
 const InitiateMongoServer = async () => {
   try {
--- server/config/keys.js
+++
@@ -1,4 +0,0 @@
-//dev.js
-module.exports = {
-    mongoURI: 'mongodb+srv://arshiahsn:iEltMewRRV1p4fp8@cluster0.8wech.mongodb.net/emaily?retryWrites=true&w=majority',
-};

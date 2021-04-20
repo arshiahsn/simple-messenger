517020c548c968d940d2e2cf7aefb89f69f445d7
Arshiahsn <arshiahsn@gmail.com>
2021-04-19 18:09:56 -0600

    refactoring

--- server/utility/sign.js
+++ server/utility/sign.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 const JWTSECRET = process.env.JWT_SECRET;
 module.exports = {
-    sign : function(payload, res){
+    sign : function(payload){
         return jwt.sign(
             payload,
             JWTSECRET,
